d812a46445b4d3528841907e5fc086cf8a9efda7
sy14r <sy14r@localhost>
2019-08-05 18:45:07 -0600

    Push

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM node:latest
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 COPY package.json /usr/src/app/
-RUN npm install
 COPY . /usr/src/app
+RUN npm install
 EXPOSE 29001
 CMD [ "ifconfig" ]
+CMD [ "npm", "install" ]
+CMD [ "npm", "rebuild" ]
 CMD [ "npm", "start" ]
--- server.js
+++ server.js
@@ -1,91 +1,183 @@
 console.log('Starting Emergence...');
 
 const express = require('express');
-const app = express();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
 const bodyParser = require('body-parser');
 const { exec } = require('child_process');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 const MongoClient = require('mongodb').MongoClient;
+const mongo = require('mongodb');
+
 var db
 
-<<<<<<< HEAD
-MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-=======
 MongoClient.connect('mongodb://mongo:27017', (err, client) => {
->>>>>>> 01d4ebd8949256d969769a14bd0f7cd524d2e98e
-  if (err) return console.log(err)
-  db = client.db('prismatica') // whatever your database name is
+  if (err) { return console.log(err); }
+  db = client.db('prismatica'); // whatever your database name is
+  // Check if default user exists and update as needed
+  const users = db.collection('users');
+  users.find({username:"admin"}).limit(1).toArray((err, result) => {
+    if (err) { console.log(err); return cb(err); }
+    if (!result) { console.log("No result found"); return cb(null, false); }
+    else {
+      if(result.length === 0) {
+        console.log('Adding default \'admin\' user');
+        bcrypt.hash('test', 10, (err, bcryptedPassword) => {
+          const defaultAdmin = {
+            username: 'admin',
+            password: bcryptedPassword
+          };
+          users.insert(defaultAdmin);
+        });
+      }
+      console.log("Successfully added default user");
+    }
+  });      
   app.listen(29001, () => {
     console.log('listening on 29001')
   })
 })
 
+passport.use(new Strategy(
+  function(username, password, cb) {
+    const sess = db.collection('users');
+    sess.find({username:username}).limit(1).toArray((err, result) => {
+      if (err) { return cb(err); }
+      if (!result) { return cb(null, false); }
+      // //to compare password that user supplies in the future
+      var hash = result[0].password;
+      bcrypt.compare(password, hash, function(err, doesMatch){
+       if (doesMatch){
+          //log in
+          return cb(null, result[0]);
+       }else{
+          //go away
+          return cb(null, false);
+       }
+      });      
+    });
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+    const sess = db.collection('users');
+    const o_id = new mongo.ObjectID(id);
+    console.log(id);
+    sess.find({"_id":o_id}).limit(1).toArray((err, result) => {
+      if (err) { return cb(err); }
+      return cb(null, result[0]);
+    });
+});
+
+const app = express();
+app.use(require('morgan')('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// NEED TO GENERATE A SECRET RANDOMLY
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', function(req, res) {
   res.send('Hello World')
 })
 
+app.get('/requires-session',
+  require('connect-ensure-login').ensureLoggedIn(),
+  (req, res) => {
+    var response = { status: "you had a valid session!"};
+    res.send(response);
+});
+
+app.post('/login', 
+passport.authenticate('local', { failureRedirect: '/login' }),
+(req, res) => {
+  var response = { status: "login success"};
+  res.send(response);
+});
+
+app.get('/login',
+(req, res) => {
+  var response = { status: "failed login"};
+  res.send(response);
+});
+
+app.post('/hash',
+  (req, res) => {
+    bcrypt.hash(req.body.data, 10, (err, bcryptedPassword) => {
+      var response = { hash: bcryptedPassword };
+      res.send(response);
+    });
+  }
+);
+
 //Get API data
 app.post('/api/get/', (req, res) => {
-   // Determine query type
-   //console.log(req.body)
-   //console.log("GET")
+  // Determine query type
+  //console.log(req.body)
+  //console.log("GET")
 
-   // If query == Get Tasks
-   if (req.body.collection == "TASK") {
+  // If query == Get Tasks
+  if (req.body.collection == "TASK") {
 
-     const sess = db.collection('SESSIONS');
-     sess.find({agentid:req.body.agentid}).limit(1).toArray(function (err, result) {
-         if (err) throw err
-         //console.log(result);
-         // IF beacon... new session?
-         if (result[0] == null) {
-           console.log("New Session Detected");
-           var sessionDetails = {
-             agentid: req.body.agentid,
-             type: "Gryf",
-             user: "ozymandias\\0zm0z1z",
-             delay: 5,
-             last: Date.now()
-           }
-           sess.save(sessionDetails, (err, result) => {
-             if (err) return console.log(err)
-          })
-        } else {
-          //Update Session Details
-          //var ll = Date.now() - result[0].last
-          var newlast = {$set: {last: Date.now()} }
-          sess.update({agentid:req.body.agentid}, newlast, function(err, res) {
-            if (err) throw err;
-          });
-        }
-     })
+    const sess = db.collection('SESSIONS');
+    sess.find({agentid:req.body.agentid}).limit(1).toArray(function (err, result) {
+        if (err) throw err
+        //console.log(result);
+        // IF beacon... new session?
+        if (result[0] == null) {
+          console.log("New Session Detected");
+          var sessionDetails = {
+            agentid: req.body.agentid,
+            type: "Gryf",
+            user: "ozymandias\\0zm0z1z",
+            delay: 5,
+            last: Date.now()
+          }
+          sess.save(sessionDetails, (err, result) => {
+            if (err) return console.log(err)
+        })
+      } else {
+        //Update Session Details
+        //var ll = Date.now() - result[0].last
+        var newlast = {$set: {last: Date.now()} }
+        sess.update({agentid:req.body.agentid}, newlast, function(err, res) {
+          if (err) throw err;
+        });
+      }
+    })
 
 
 
-     // Get tasks for agentId
-     const collection = db.collection('TASK');
-     collection.find({agentid:req.body.agentid}).limit(1).sort({_id: -1}).toArray(function (err, result) {
-         if (err) throw err
-         //console.log(result[0])
-         res.json(result[0])
-     })
-   }
-   else {
-      res.send({});
-   }
- })
- //TASK Agent
- app.post('/api/task/', (req, res) => {
-   console.log(req.body)
-   const collection = db.collection('TASK');
-   collection.save(req.body, (err, result) => {
-     if (err) return console.log(err)
+    // Get tasks for agentId
+    const collection = db.collection('TASK');
+    collection.find({agentid:req.body.agentid}).limit(1).sort({_id: -1}).toArray(function (err, result) {
+        if (err) throw err
+        //console.log(result[0])
+        res.json(result[0])
+    })
+  }
+  else {
+    res.send({});
+  }
+})
+
+//TASK Agent
+app.post('/api/task/', (req, res) => {
+  console.log(req.body)
+  const collection = db.collection('TASK');
+  collection.save(req.body, (err, result) => {
+    if (err) return console.log(err)
   })
-   res.send({});
- })
+  res.send({});
+})
 
 //Set API data
 app.post('/api/update/', (req, res) => {
@@ -93,39 +185,41 @@ app.post('/api/update/', (req, res) => {
   console.log(req.body)
   collection.insertOne(req.body, (err, result) => {
     if (err) return console.log(err)
- })
+  })
   res.send({});
 })
+
 //Get API data
 app.post('/api/c2/', (req, res) => {
-   // Determine query type
-   //console.log(req.body)
-   //console.log("C2")
+  // Determine query type
+  //console.log(req.body)
+  //console.log("C2")
 
-   // If query == Get Tasks
-   if (req.body.collection == "C2") {
-      // Get tasks for agentId
-      const collection = db.collection('C2');
-      collection.find().limit(1).sort({_id: -1}).toArray(function (err, result) {
-          if (err) throw err
-          //console.log(result[0])
-          res.json(result[0])
-      })
-   }
-   else {
-      res.send({});
-   }
- })
- //Session Data
- app.post('/api/sessions/', (req, res) => {
-   //console.log(req.body)
-   const collection = db.collection('SESSIONS');
-   collection.find().toArray(function (err, result) {
-       if (err) throw err
-       //console.log(result)
-       res.json(result)
-   })
- })
+  // If query == Get Tasks
+  if (req.body.collection == "C2") {
+    // Get tasks for agentId
+    const collection = db.collection('C2');
+    collection.find().limit(1).sort({_id: -1}).toArray(function (err, result) {
+        if (err) throw err
+        //console.log(result[0])
+        res.json(result[0])
+    })
+  }
+  else {
+    res.send({});
+  }
+})
+
+//Session Data
+app.post('/api/sessions/', (req, res) => {
+  //console.log(req.body)
+  const collection = db.collection('SESSIONS');
+  collection.find().toArray(function (err, result) {
+      if (err) throw err
+      //console.log(result)
+      res.json(result)
+  })
+})
 
 //Add junk to db
 app.post('/stockdata', (req, res) => {
